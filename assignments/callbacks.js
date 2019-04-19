82b1688bda120e6cd30bb45d23cb8031437c991d
ShaunKolich <employmeinc@gmail.com>
2019-04-18 22:42:22 -0400

    Completed two problems on the JS Callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,10 +2,10 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
-  //Given this problem: 
-  
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -17,21 +17,36 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     return cb(arr[0]);
   }
 
-  // Function invocation 
+  // Function invocation
   firstItem(items, function(first) {
     console.log(first)
   });
 
 */
-
-
-function getLength(arr, cb) {
+function getLength(array, getLengthCb) {
+  console.log(getLengthCb(array));
+  // console.log(`The length of array is ${results}`);
   // getLength passes the length of the array into the callback.
 }
 
-function last(arr, cb) {
+let getLengthCb = (array) => {
+  return array.length;
+}
+// console.log(results);
+
+// getLength(items, getLengthCb);
+
+let getLastCb = (lastItem) => {
+  // array.length - 1;
+  return lastItem;
+}
+
+function last(array, getLastCb) {
+  console.log(getLastCb(array[array.length - 1]));
+  console.log(`Last Item Is ${getLastCb(array[array.length - 1])}`);
   // last passes the last item of the array into the callback.
 }
+last(items, getLastCb);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
